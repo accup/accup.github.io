13fd3b1249ed706c147bd391984f4940a8189868
accup <32096121+accup@users.noreply.github.com>
2022-10-15 23:54:43 +0900

    chore: install and configure vanilla-extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vanillaExtractPlugin()],
   appType: "spa",
 });
