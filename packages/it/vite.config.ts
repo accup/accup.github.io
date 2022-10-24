22e829e6cd17b51dbc70db8d49236b56132c5654
accup <32096121+accup@users.noreply.github.com>
2022-10-25 01:32:49 +0900

    chore: use vite-plugin-emit-dts

--- packages/it/vite.config.ts
+++ packages/it/vite.config.ts
@@ -1,52 +1,15 @@
 import { defineConfig } from "vite";
+import emitDts from "@accup/vite-plugin-emit-dts";
 import path from "node:path";
-import ts from "typescript";
-
-const tsOptions: ts.CompilerOptions = {
-  target: ts.ScriptTarget.ESNext,
-  module: ts.ModuleKind.ESNext,
-  moduleResolution: ts.ModuleResolutionKind.NodeJs,
-  strict: true,
-  jsx: ts.JsxEmit.Preserve,
-  resolveJsonModule: true,
-  isolatedModules: true,
-  esModuleInterop: true,
-  lib: ["ESNext", "DOM"],
-  strictNullChecks: true,
-  declaration: true,
-  declarationDir: "types",
-  preserveValueImports: true,
-  preserveWatchOutput: true,
-};
 
 export default defineConfig({
-  plugins: [
-    {
-      name: "tsc",
-      buildStart(options) {
-        const host = ts.createCompilerHost(tsOptions);
-        host.writeFile = (fileName: string, source: string) => {
-          this.emitFile({
-            type: "asset",
-            fileName,
-            source,
-          });
-        };
-        const inputFileNames = Array.isArray(options.input)
-          ? options.input.map((entry) => entry)
-          : Object.entries(options.input).map(([, fileName]) => fileName);
-        const program = ts.createProgram(inputFileNames, tsOptions, host);
-        program.emit(undefined, undefined, undefined, true, undefined);
-      },
-    },
-  ],
+  plugins: [emitDts()],
   build: {
     outDir: "dist",
     lib: {
       entry: path.resolve(__dirname, "./src/index.ts"),
       fileName: "it",
       name: "It",
-      formats: ["es", "umd"],
     },
   },
 });
