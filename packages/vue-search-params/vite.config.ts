aeddc88653b9500fb334cedf9669e418238b2c55
accup <32096121+accup@users.noreply.github.com>
2022-10-26 02:27:03 +0900

    chore: not empty in watch mode

--- packages/vue-search-params/vite.config.ts
+++ packages/vue-search-params/vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import emitDts from "@accup/vite-plugin-emit-dts";
 import path from "node:path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [emitDts()],
   build: {
     outDir: "dist",
+    emptyOutDir: mode !== "development",
     lib: {
       entry: path.resolve(__dirname, "./src/index.ts"),
       fileName: "vue-search-params",
       name: "VueSearchParams",
     },
   },
-});
+}));
