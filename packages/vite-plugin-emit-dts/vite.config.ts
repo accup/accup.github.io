97e68986bbf10e747c5b4b05aef9b7e3e0364e08
accup <32096121+accup@users.noreply.github.com>
2022-10-26 02:26:30 +0900

    fix(emit-dts): avoid to bundle library

--- packages/vite-plugin-emit-dts/vite.config.ts
+++ packages/vite-plugin-emit-dts/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import path from "node:path";
 import emitDts from "./src/index";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [emitDts()],
   build: {
     outDir: "dist",
+    emptyOutDir: command === "build",
     lib: {
       entry: path.resolve(__dirname, "./src/index.ts"),
       fileName: "vite-plugin-emit-dts",
@@ -13,7 +14,13 @@ export default defineConfig({
       formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["vite", "typescript", "node:fs", "node:path"],
+      external: [
+        "vite",
+        "typescript",
+        "@microsoft/api-extractor",
+        "node:fs",
+        "node:path",
+      ],
     },
   },
-});
+}));
