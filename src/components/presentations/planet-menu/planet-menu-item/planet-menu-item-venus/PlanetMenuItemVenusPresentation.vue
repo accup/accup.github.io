8ed969a72a9eadd9ead7366984a2144553640e78
accup <32096121+accup@users.noreply.github.com>
2023-06-21 16:24:03 +0900

    [feat] add venus

---
+++ src/components/presentations/planet-menu/planet-menu-item/planet-menu-item-venus/PlanetMenuItemVenusPresentation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PlanetMenuItem from "../@components/PlanetMenuItem.vue";
+
+import fragmentShader from "./@shaders/PlanetMenuItemVenus.frag?raw";
+
+import { input, label } from "./PlanetMenuItemVenusPresentation.css";
+</script>
+
+<template>
+  <PlanetMenuItem
+    :orbital-inclination-degree="7.01"
+    :axial-tilt-degree="0.01"
+    :rotation-period-days="58.7"
+    :fragment-shader="fragmentShader"
+  >
+    <label :class="label">
+      <span>金星</span>
+      <input :class="input" type="text" pattern="[\d,]+" />
+    </label>
+  </PlanetMenuItem>
+</template>
