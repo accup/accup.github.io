ad9aaa26cf7d06234f6b8fe180d154fabe0638bb
accup <32096121+accup@users.noreply.github.com>
2023-06-24 18:08:33 +0900

    [fix] optimize layout

--- src/layouts/DefaultLayout.astro.css.ts
+++ src/layouts/DefaultLayout.astro.css.ts
@@ -2,12 +2,9 @@ import { style } from "@vanilla-extract/css";
 
 import { recipes } from "../themes-astro/recipes.astro.css";
 
-export const root = style([
+export const body = style([
   recipes({
     font: "sans-04",
     text: "text-01",
   }),
-  {
-    display: "flow-root",
-  },
 ]);
